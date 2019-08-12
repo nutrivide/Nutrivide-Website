352749e56606ff4fd0802e688b3e0cbcda502c69
advil64 <advil64@gmail.com>
2019-08-12 00:13:02 -0400

    modified navbar and included scrollspy functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 
 jQuery(document).ready(function($){
     
+    var topoffset = 55; //variable for menu height
+
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar-nav a').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset + 2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
+    
+    $(window).on('activate.bs.scrollspy', function(){
+        
+        var hash = $('.site-nav').find('a.active').attr('href');
+        
+        if(hash !== '#page-hero'){
+            $('header nav').addClass('inbody');
+        }else{
+            $('header nav').removeClass('inbody');
+        }
+    });
+
+    
     var timelines = $('.cd-horizontal-timeline'),
 		eventsMinDistance = 60;
 
